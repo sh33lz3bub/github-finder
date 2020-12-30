b0a69027a4847216e62fd399b010e6b1452e5d04
sh33lz3bub <shelby@goerlitz.us>
2020-12-29 19:27:04 -0800

    2 more build errors cleaned up

--- src/context/alert/AlertState.js
+++ src/context/alert/AlertState.js
@@ -2,7 +2,6 @@ import React, { useReducer } from 'react';
 import AlertContext from './alertContext';
 import AlertReducer from './alertReducer';
 import { SET_ALERT, REMOVE_ALERT} from '../types';
-import { findAllByTestId } from '@testing-library/react';
 
 const AlertState = props => {
   const initialState = null;
