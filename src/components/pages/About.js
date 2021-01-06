c9679027c364d90c60838c3eaa8b5081b4f1cd69
sh33lz3bub <shelby@goerlitz.us>
2021-01-05 18:07:45 -0800

    fixed about page "a"

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,7 +3,7 @@ import React, { Fragment } from 'react'
 const About = () => {
   return (
     <Fragment>
-      <h1>About This 4pp</h1>
+      <h1>About This App</h1>
       <p>App to search Github users</p>
       <p>Version: 1.0.0</p>
     </Fragment>
